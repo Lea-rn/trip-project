efbb316d082853672610791781f3fbda1bf793a9
achrefmechergui <achreffcb123achref@gamil.com>
2025-05-03 22:20:03 +0100

    controlled element

--- trip-app/src/App.js
+++ trip-app/src/App.js
@@ -1,52 +1,106 @@
+import { useState } from "react";
 import "./App.css";
-import "./style.css"
+import "./style.css";
+
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 1,
+    packed: false,
+  },
+
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 1, packed: true },
+];
 
 function App() {
-  return <div>
-    <Logo/>
-    <Form/>
-    <PackingList/>
-    <Stats/>
-  </div>
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 function Logo() {
- return <header>
-  <h1> 🌴 Far Away 🎒</h1>
- </header>
+  return (
+    <header>
+      <h1> 🌴 Far Away 🎒</h1>
+    </header>
+  );
 }
+
+
 
 function Form() {
-  return (
-    <div className="form-container">
-    <h3>What do you need for your 😊 trip ?</h3>
-  <select>
-    <option>1</option>
-     <option>2</option>
-     <option>3</option>
-  </select>
-  <input type="text"/>
-  <button>submit</button>
-  </div>
-  )
 
+   const [description , setDescription] = useState ("")
+function handleSubmit (event){
+  console.log(event)
+event.preventDefault()
+  console.log("test")
 }
+
+  return (
+    
+    <div className="form-container">
+      <h3>What do you need for your 😊 trip ?</h3>
+      <form className="form-element" onSubmit={handleSubmit} >
+        <select>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option key={num}  value={num}> {num}</option>
+          ))}
+        </select>
+        <input placeholder="item..." type="text" value={description}
+       onChange={(e)=> setDescription(e.target.value)}
+        />
+     <button>ADD</button>
+      </form>
+    </div>
+  );
+  
+}
+
 
 function PackingList() {
   return (
     <div className="list">
-  LIST
-    </div>
-  )
+      {initialItems.map((item) => (
+        <Item key={item.id} obj={item} />
 
+
+      ))}
+    </div>
+  );
+}
+
+
+
+
+function Item({ obj }) {
+  return (
+    <div>
+      <span className="element">{obj.quantity}</span>
+      <span style={obj.packed ? { textDecoration: "line-through" } : {}}>
+        {obj.description}
+      </span>
+      <span className="btn-delete">❌</span>
+   
+    </div>
+  );
 }
 
 function Stats() {
   return (
     <footer>
-      <strong>🎒 You have X items on your list , and you already packed X (%X)</strong>
+      <strong>
+        🎒 You have X items on your list , and you already packed X (%X)
+      </strong>
     </footer>
-  )
+  );
 }
 
 export default App;
