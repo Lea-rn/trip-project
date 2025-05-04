c3b1f06b783a7b9bf22eec83b4f91b845965a627
achrefmechergui <achreffcb123achref@gamil.com>
2025-05-04 21:21:34 +0100

    controlled element

--- trip-app/src/App.js
+++ trip-app/src/App.js
@@ -33,52 +33,64 @@ function Logo() {
   );
 }
 
-
-
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
-   const [description , setDescription] = useState ("")
-function handleSubmit (event){
-  console.log(event)
-event.preventDefault()
-  console.log("test")
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!description) return ; 
+    const newItem = {
+      description: description,
+      quantity: quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    console.log(newItem);
+
+    setDescription("")
+    setQuantity("")
+  }
 
   return (
-    
     <div className="form-container">
       <h3>What do you need for your 😊 trip ?</h3>
-      <form className="form-element" onSubmit={handleSubmit} >
-        <select>
+      <form className="form-element" onSubmit={handleSubmit}>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
           {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-            <option key={num}  value={num}> {num}</option>
+            <option key={num} value={num}>
+              {num}
+            </option>
           ))}
         </select>
-        <input placeholder="item..." type="text" value={description}
-       onChange={(e)=> setDescription(e.target.value)}
+{/* 
+        <input  placeholder="quantity ..." type="number" value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}/> */}
+        <input
+          placeholder="item..."
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
-     <button>ADD</button>
+        <button>ADD</button>
       </form>
     </div>
   );
-  
 }
-
 
 function PackingList() {
   return (
     <div className="list">
       {initialItems.map((item) => (
         <Item key={item.id} obj={item} />
-
-
       ))}
     </div>
   );
 }
-
-
-
 
 function Item({ obj }) {
   return (
@@ -88,7 +100,6 @@ function Item({ obj }) {
         {obj.description}
       </span>
       <span className="btn-delete">❌</span>
-   
     </div>
   );
 }
